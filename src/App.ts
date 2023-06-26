2fb9561c3845e45b59115843c02e33426f9b9e18
Prithviraaj <z5419216@ad.unsw.edu.au>
2023-06-26 14:45:34 +1000

    init

---
+++ src/App.ts
@@ -0,0 +1,5 @@
+
+import { app, BrowserWindow } from 'electron';
+import Main from './Main';
+
+Main.main(app, BrowserWindow);
---
+++ src/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build the App
+npm run build
+
+# Start the App
+npm run start
